79b995992cac7d34aaa3ca76fa4f16c6b94bfaf3
Anna Bogomazova <annabogomazova1990@gmail.com>
2026-02-15 13:02:10 +0200

    reviews

--- js/index.js
+++ js/index.js
@@ -5,3 +5,24 @@ hamburger.addEventListener('click', function() {
     menu.classList.toggle('active');
 });
 
+//slider
+const catalog = document.querySelector('.trending__catalog');
+const btnPrev = document.querySelector('.trending__btn-prev');
+const btnNext = document.querySelector('.trending__btn-next');
+//создала переменные и сохранила елементы страницы которые нахожу через document.querySelector по классам
+
+// ширина прокрутки = ширина одной карточки
+const cardWidth = document.querySelector('.trending__catalog-card').offsetWidth + 32;
+// +32 потому что у тебя gap: 32px
+
+
+//обработчик событий
+//На кнопку btnNext вешаю обработчик события click.
+btnNext.addEventListener('click', () => {
+    catalog.scrollLeft += cardWidth ; // листаем на карточку
+});
+
+btnPrev.addEventListener('click', () => {
+    catalog.scrollLeft -= cardWidth ;
+});
+
