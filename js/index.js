b8187c1e3622a033f0705ad429c93e191b3441dd
Anna Bogomazova <annabogomazova1990@gmail.com>
2026-02-23 12:40:45 +0200

    home-bimage

--- js/index.js
+++ js/index.js
@@ -28,23 +28,27 @@ btnPrev.addEventListener('click', () => {
 
 //slider reviews-section
 
-    // находим все слайды и точки
-
-
     const slides = document.querySelectorAll('.reviews__item');
     const dots = document.querySelectorAll('.dot');
      for (let i = 0; i < dots.length; i++) {
     dots[i].onclick = function() {
-
 
         // скрываем все слайды
         for (let j = 0; j < slides.length; j++) {
             slides[j].classList.remove('active');
             dots[j].classList.remove('active');
         }
-
         // показываем выбранный
         slides[i].classList.add('active');
         dots[i].classList.add('active');
     }
 }
+
+const button = document.querySelector('.home-banner__btn'); // кнопка
+const features = document.querySelectorAll('.home-banner__feature'); // все иконки
+
+button.addEventListener('click', function() {
+    features.forEach(feature => {
+        feature.classList.toggle('remove'); // скрываем или показываем каждую иконку
+    });
+});
